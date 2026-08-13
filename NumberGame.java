353aa9d548fda3b5bdc044c7003ef07bdc942070
vaibhav sharma <vaibhavsharma3452@gmail.com>
2026-08-13 17:01:14 +0530

    NumGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -1,56 +1,46 @@
+ 
+
+import java.util.Scanner; 
 import java.util.Random;
-import java.util.Scanner;
-
-public class NumberGame {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Random rand = new Random();
-        int score = 0; // counts how many rounds the user wins
-        String playAgain;
-
-        System.out.println("🎯 Welcome to the Number Guessing Game! 🎯");
-
-        do {
-            int lowerBound = 1;
-            int upperBound = 100;
-            int randomNumber = rand.nextInt(upperBound - lowerBound + 1) + lowerBound;
-            int attempts = 0;
-            int maxAttempts = 7; // limit attempts per round
-            boolean isCorrect = false;
-
-            System.out.println("\nI have selected a number between " + lowerBound + " and " + upperBound + ".");
-            System.out.println("You have " + maxAttempts + " attempts to guess it!");
-
-            while (attempts < maxAttempts) {
-                System.out.print("\nEnter your guess: ");
-                int userGuess = sc.nextInt();
-                attempts++;
-
-                if (userGuess == randomNumber) {
-                    System.out.println("🎉 Correct! You guessed the number in " + attempts + " attempts!");
-                    score++;
-                    isCorrect = true;
-                    break;
-                } else if (userGuess < randomNumber) {
-                    System.out.println("📉 Too low! Try again.");
-                } else {
-                    System.out.println("📈 Too high! Try again.");
-                }
+ public class NumGame {
+    public static void main(String ar[])
+    {
+        Scanner s = new Scanner(System.in);
+        Random r = new Random();
+         int numtoguess = r.nextInt(101);
+         int max_tries = 4; 
+         int guess ;
+         int score = 0 ;
+         int numoftries = 0 ;
+         boolean guesscorrect = false;
+         System.out.println("wellcome to NUMBER GAME");
+         System.out.println("You will get "+ max_tries + "to win the game" );
+         System.out.println("guess the number between 1 to 100 only ");
+         while(!guesscorrect &&  numoftries<max_tries) {
+            System.out.println("enter your guessed number");
+            guess = s.nextInt();
+            numoftries++;
+               
+            if(guess < numtoguess){
+                System.out.println("too low.. ,   plz try again");
             }
-
-            if (!isCorrect) {
-                System.out.println("❌ You've used all attempts! The correct number was: " + randomNumber);
+            else if(guess > numtoguess){
+                System.out.println(" too high.. , try again ");
             }
+            else {
+                guesscorrect = true;
+                score = 100 - (numoftries - 1)*15;
+                System.out.println("You guessed number in " + numoftries + "tries");
+                System.out.println("your score is " + score);
+            } 
+         }
+         if(!guesscorrect){
+            System.out.println("you out and ur score is 0");
+            System.out.print("guessed number was" +numtoguess );
 
-            System.out.print("\nDo you want to play another round? (yes/no): ");
-            playAgain = sc.next();
+         }
 
-        } while (playAgain.equalsIgnoreCase("yes"));
-
-        System.out.println("\n🏁 Game Over!");
-        System.out.println("✅ Your Final Score (Rounds Won): " + score);
-        System.out.println("Thanks for playing! 😊");
-
-        sc.close();
     }
+
+    
 }
